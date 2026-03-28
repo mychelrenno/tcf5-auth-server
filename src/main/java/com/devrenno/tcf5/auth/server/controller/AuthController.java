7f21a26a74aaa23caf9ed6cf141554c9a9ca476e
Mychel Renno <mychelrenno@gmail.com>
2026-03-28 15:52:43 -0300

    feat - ajustado endpoints

--- src/main/java/com/devrenno/tcf5/auth/server/controller/AuthController.java
+++ src/main/java/com/devrenno/tcf5/auth/server/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.devrenno.tcf5.auth.server.controller;
 
 import com.devrenno.tcf5.auth.server.entity.Client;
 import com.devrenno.tcf5.auth.server.service.ClientService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +19,14 @@ public class AuthController {
     }
 
     @GetMapping("/{id}")
-    public Client findById(@PathVariable String id) {
-        return clientService.findById(id);
+    public ResponseEntity<Client> findById(@PathVariable String id) {
+        var client = clientService.findById(id);
+        return ResponseEntity.ok(client);
     }
 
     @GetMapping("/validate/{id}")
-    public boolean validade(@PathVariable String id) {
-        return clientService.validade(id);
+    public ResponseEntity<Boolean> validade(@PathVariable String id) {
+        var isValid = clientService.validade(id);
+        return ResponseEntity.ok(isValid);
     }
 }
--- src/main/java/com/devrenno/tcf5/auth/server/entity/Client.java
+++ src/main/java/com/devrenno/tcf5/auth/server/entity/Client.java
@@ -12,4 +12,29 @@ public class Client {
     private String razaoSocial;
 
     private String cnpj;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
 }
