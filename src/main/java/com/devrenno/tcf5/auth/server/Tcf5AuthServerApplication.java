7145c28866b521b50576eb31b5713147a834be87
Mychel Renno <mychelrenno@gmail.com>
2026-03-28 11:45:07 -0300

    criado projeto inicial

---
+++ src/main/java/com/devrenno/tcf5/auth/server/Tcf5AuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.devrenno.tcf5.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tcf5AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tcf5AuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devrenno/tcf5/auth/server/Tcf5AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrenno.tcf5.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tcf5AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
